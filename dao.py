d608eac452a9332d255531976637c671866085c9
Israel Braitt <59903080+israelbraitt@users.noreply.github.com>
2023-03-11 00:34:03 -0300

    Update message treatment
    Update messages treatment method from server.py and search methods from dao.py

--- dao.py
+++ dao.py
@@ -5,6 +5,17 @@ class DAO:
 
     @staticmethod
     def getClient(username, registration_number):
+        """
+        Confere se o cliente está cadastrado ou não na base de dados
+
+            Parâmetros:
+                username (str): nome de usuário
+                registration_number (str): número de matrícula do usuário
+            
+            Retornos:
+                True (bool): caso os dados do cliente estejam cadastrados
+                False (bool): caso os dados do cliente não estejam cadastrados
+        """
         if (username != ""):
             file = open("database/dados_clientes.txt")
             lines = file.readlines()
@@ -32,6 +43,16 @@ class DAO:
 
     @staticmethod        
     def getLastMeasurement(registration_number):
+        """
+        Retorna os dados da última medição associada a determinada matrícula
+
+            Parâmetros:
+                registration_number (str): número de matrícula do usuário
+            
+            Retornos:
+                (date_time, consumption) (tuple): data, hora e consumo registrados na última medição
+                (0, 0) (tuple): caso não tenha nenhuma medição associada ao número de matrícula
+        """
         if (registration_number != ""):
             file = open("database/medicoes.txt")
             lines = file.readlines()
@@ -40,24 +61,68 @@ class DAO:
                 if (registration_line == registration_number):
                     date_time = line.split(";")[0]
                     consumption = line.split(";")[2]
+                    consumption = consumption.split("\n")[0]
                     return (date_time, consumption)
             file.close()
             return (0, 0)
 
     @staticmethod    
-    def get5LastMeasurements(registration_number):
+    def get2LastMeasurements(registration_number):
+        """
+        Retorna os dados das 2 últimas medições associadas a determinada matrícula
+
+            Parâmetros:
+                registration_number (str): número de matrícula do usuário
+            
+            Retornos:
+               measurement_list (list[tuple]): lista contendo as 2 últimas medições
+        """
         measurement_list = []
         list_itens = 0
-        while list_itens < 6:
-            if (registration_number != ""):
-                file = open("database/medicoes.txt")
-                lines = file.readlines()
-                for line in lines:
-                    registration_line = line.split(";")[1]
-                    if (registration_line == registration_number):
-                        date_time = line.split(";")[0]
-                        consumption = line.split(";")[2]
-                        measurement_list.append((date_time, consumption))
-                file.close()
-                return measurement_list
+        if (registration_number != ""):
+            file = open("database/medicoes.txt")
+            lines = file.readlines()
+            for line in lines:
+                registration_line = line.split(";")[1]
+                if (registration_line == registration_number):
+                    date_time = line.split(";")[0]
+                    consumption = line.split(";")[2]
+                    consumption = consumption.split("\n")[0]
+                    measurement_list.append((date_time, consumption))
+                    
+                    list_itens += 1
+                    if (list_itens == 2):
+                        break;
+        file.close()
+        return measurement_list
+
+    @staticmethod    
+    def get5LastMeasurements(registration_number):
+        """
+        Retorna os dados das 5 últimas medições associadas a determinada matrícula
+
+            Parâmetros:
+                registration_number (str): número de matrícula do usuário
+            
+            Retornos:
+               measurement_list (list[tuple]): lista contendo as 5 últimas medições
+        """
+        measurement_list = []
+        list_itens = 0
+        if (registration_number != ""):
+            file = open("database/medicoes.txt")
+            lines = file.readlines()
+            for line in lines:
+                registration_line = line.split(";")[1]
+                if (registration_line == registration_number):
+                    date_time = line.split(";")[0]
+                    consumption = line.split(";")[2]
+                    consumption = consumption.split("\n")[0]
+                    measurement_list.append((date_time, consumption))
+
+                    list_itens += 1
+                    if (list_itens == 5):
+                        break;
+        file.close()
+        return measurement_list
     
--- server.py
+++ server.py
@@ -10,17 +10,17 @@ BUFFER_SIZE = 2048
 
 clients = []
 
-# taxa de consumo de energia
+# Taxa de consumo de energia
 CONSUMPTION_RATE = 3
 
 def main():
-    # os parâmetros do método socket indicam a família de protocolo (IPV4)
-    # e o tipo do protocolo (TCP)
+    # Cria um socket com conexão TCP e outro com conexão UDP
     server_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
     try:
-        # vinculado os sockets dos clientes ao endereço do servidor
+        # Fornece o endereço e as portas para "escutar" as conexões
+        # com os sockets dos clientes
         server_tcp.bind(('', TCP_PORT))
         server_tcp.listen()
 
@@ -30,23 +30,30 @@ def main():
         return print("Não foi possível iniciar o servidor")
 
     while True:
-        # aceita a conexão com os sockets dos clientes
+        # Aceita a conexão com os sockets dos clientes
         # conn_client é do tipo 'socket.socket' e addr_client é do tipo 'tupla'
         conn_client_tcp, addr_client_tcp = server_tcp.accept()
         print("Conectado em", addr_client_tcp)
         clients.append(conn_client_tcp)
-
-        conn_client_udp, addr_client_udp = server_udp.accept()
-        print("Conectado em", addr_client_udp)
-        clients.append(conn_client_udp)
-
-        thread1 = threading.Thread(target=messagesTreatment, args=[conn_client_tcp])
-        thread2 = threading.Thread(target=messagesTreatment, args=[conn_client_udp])
         
-        thread1.start()
-        thread2.start()
+        # Recebe mensagens dos clientes através da conexão TCP
+        thread_tcp = threading.Thread(target=messagesTreatment, args=[conn_client_tcp])
+        thread_tcp.start()
+
+        # Recebe mensagens dos clientes através da conexão UDP
+        message_udp, addr_client_udp = server_udp.recvfrom(BUFFER_SIZE)
+        print("Conectado em", addr_client_udp)
 
 def messagesTreatment(client):
+    """
+    Faz o tratamento dos "requests" dos clientes
+
+        Parâmetros:
+            client (socket.socket): cliente conectado
+        
+        Retornos:
+
+    """
     while True:
         try:
             message = client.recv(BUFFER_SIZE)
@@ -54,87 +61,105 @@ def messagesTreatment(client):
             print("Mensagem recebida:", message)
             print("data", data)
             
-
             if (data["method"] == "GET"):
                 if (data["url_content"] == "/validacao-usuario"):
                     username = data["body_content"]["username"]
                     registration_number = data["body_content"]["registration"]
                     
+                    # Consulta se o cliente está registrado na base de dados
                     dao_inst = DAO()
                     validation_client = dao_inst.getClient(username, registration_number)
 
                     if (validation_client == True):
-                        request = assembleResponse(200, "OK")
-                        sendMessages(request, client)
+                        request = assembleResponse("200", "OK")
+                        sendMessages(client, request)
 
                     elif (validation_client == False):
-                        request = assembleResponse(404, "Not Found")
+                        request = assembleResponse("404", "Not Found")
                         sendMessages(request, client)
                         deleteClient(client)
                 
                 elif (data["url_content"] == "/medicoes/ultima-medicao"):
                     registration_number = data["body_content"]["registration"]
 
+                    # Consulta a última medição associada a determinado número de matrícula
                     dao_inst = DAO()
                     last_measurement = dao_inst.getLastMeasurement(registration_number)
 
                     if (last_measurement != (0, 0)):
+                        # Retorna a data, a hora e o consumo registrado na última medição
                         date_time = last_measurement[0]
                         consumption = last_measurement[1]
                         dic_last_measurement = { "date_time" : date_time, "consumption" : consumption}
                         
-                        request = assembleResponse(200, json.dumps(dic_last_measurement))
-                        sendMessages(request, client)
+                        request = assembleResponse("200", json.dumps(dic_last_measurement))
+                        sendMessages(client, request)
                     else:
-                        request = assembleResponse(404, "Not Found")
-                        sendMessages(request, client)
+                        request = assembleResponse("404", "Not Found")
+                        sendMessages(client, request)
 
                 elif (data["url_content"] == "/gerar-fatura"):
                     registration_number = data["body_content"]["registration"]
 
+                    # Consulta as 2 últimas medições associadas a determinado número de matrícula
                     dao_inst = DAO()
-                    last_measurements = dao_inst.get5LastMeasurements(registration_number)
-
-                    
-                    if (last_measurements[0] != (0, 0) and last_measurements[1] != (0, 0)):
-                        consumption_final = last_measurements[0][1]
-                        consumption_inicial = last_measurements[1][1]
-                        consumption_total = consumption_final - consumption_inicial
-
-                        amount_payment = consumption_total * CONSUMPTION_RATE
+                    last_2_measurements = dao_inst.get2LastMeasurements(registration_number)
+                    print(last_2_measurements)
+                    if (last_2_measurements[0] != ('0', '0') and last_2_measurements[1] != ('0', '0')):
+                        date, final_consumption = last_2_measurements[0]
+                        date, inicial_consumption = last_2_measurements[1]
+                        total_consumption = int(final_consumption) - int(inicial_consumption)
                         
-                        dic_invoice = { "consumption" : consumption_total , "amount_payment" : amount_payment}
-
-                        request = assembleResponse(202, json.dumps(dic_invoice))
-                        sendMessages(request, client)
+                        # Multiplica o total de consumo do último período registrado
+                        # pelo valor da taxa de consumo
+                        amount_payment = total_consumption * CONSUMPTION_RATE
+                        
+                        dic_invoice = { "consumption" : total_consumption , "amount_payment" : amount_payment}
+                        
+                        request = assembleResponse("202", json.dumps(dic_invoice))
+                        sendMessages(client, request)
                     else:
-                        request = assembleResponse(404, "Not Found")
-                        sendMessages(request, client)
+                        request = assembleResponse("404", "Not Found")
+                        sendMessages(client, request)
 
                 elif (data["url_content"] == "/alerta-consumo"):
                     registration_number = data["body_content"]["registration"]
-
-                    dao_inst = DAO()
-                    last_measurements = dao_inst.get5LastMeasurements(registration_number)
-
                     
-                    if (last_measurements[0] != (0, 0) and last_measurements[1] != (0, 0)):
+                    # Consulta as 5 últimas medições associadas a determinado número de matrícula
+                    dao_inst = DAO()
+                    last_5_measurements = dao_inst.get5LastMeasurements(registration_number)
+
+                    if (last_5_measurements[0] != (0, 0) and last_5_measurements[1] != (0, 0)):
                         variation_consumption_list = []
                         i = 4
                         while i > 0:
-                            consumption_final = last_measurements[i][1]
-                            consumption_inicial = last_measurements[i-1][1]
-                            consumption_total = consumption_final - consumption_inicial
-                            variation_consumption_list.append(consumption_total)
+                            date, final_consumption = last_5_measurements[i-1]
+                            date, inicial_consumption = last_5_measurements[i]
+                            total_consumption = int(final_consumption) - int(inicial_consumption)
+                            # Calcula e salva a variação de consumo dos último 4 meses
+                            variation_consumption_list.append(total_consumption)
                             i -= 1
                         
-                        dic_invoice = { "consumption" : consumption_total }
+                        # Calcula a média de consumo dos últimos 3 meses anteriores
+                        media = (variation_consumption_list[0] + 
+                                variation_consumption_list[1] +
+                                variation_consumption_list[2])/3
+                        
+                        if (variation_consumption_list[3] >= media*(1,5)):
+                            # Calcula a diferença de consumo do último mês em relação
+                            # à média de consumo dos últimos 3 meses anteriores
+                            overconsumption = variation_consumption_list[3] - media
+                            dic_invoice = { "overconsumption" : overconsumption }
 
-                        request = assembleResponse(200, json.dumps(dic_invoice))
-                        sendMessages(request, client)
+                            request = assembleResponse("200", json.dumps(dic_invoice))
+                            sendMessages(client, request)
+                        else:
+                            request = assembleResponse("200", "Sem consumo excessivo")
+                            print(request)
+                            sendMessages(client, request)
                     else:
-                        request = assembleResponse(404, "Not Found")
-                        sendMessages(request, client)
+                        request = assembleResponse("404", "Not Found")
+                        sendMessages(client, request)
 
             elif (data["method"] == "POST"):
                 pass
@@ -146,27 +171,65 @@ def messagesTreatment(client):
             deleteClient(client)
             break
 
-def sendMessages(msg, client):
+def sendMessages(client, message):
+    """
+    Envia mensagens para determinado cliente
+
+        Parâmetros:
+            message (str): mensagem a ser enviada ("response")
+            client (socket.socket): cliente conectado
+        
+        Retornos:
+
+    """
     try:
-        client.send(msg.encode('utf-8'))
+        client.send(message.encode('utf-8'))
     except:
         deleteClient(client) 
 
-def broadcast(msg, client):
+def broadcast(client, message):
+    """
+    Envia mensagens para todos os clientes na lista de clientes, exceto
+    para o que enviou a mensagem
+
+        Parâmetros:
+            message (str): mensagem a ser enviada ("response")
+            client (socket.socket): cliente conectado
+        
+        Retornos:
+
+    """
     for client_item in clients:
         if (client_item != client):
             try:
-                client_item.send(msg)
+                client_item.send(message.encode('utf-8'))
             except:
                 deleteClient(client_item)
 
 def deleteClient(client):
+    """
+    Remove um cliente da lista de clientes
+
+        Parâmetros:
+            client (socket.socket): cliente conectado
+        
+        Retornos:
+
+    """
     clients.remove(client)
 
 def getMessageData(message):
+    """
+    Obtém os dados de uma "request"
+
+        Parâmetros:
+            message (str): mensagem recebida ("request")
+        
+        Retornos:
+            um dicionário com o "method", o "url_content" e o "body_content" da "request"
+    """
     method = message.split(" ")[0]
     url_content = message.split(" ")[1]
-
 
     try:    
         # Prepara as mensagens no padrão JSON
@@ -188,6 +251,16 @@ def getMessageData(message):
     }
 
 def assembleResponse(status_code, status_message):
+    """
+    Monta a "response" a ser enviada
+
+        Parâmetros:
+            status_code (str): código de status da resposta HTTP do servidor
+            status_message (str): mensagem de status da resposta do servidor
+        
+        Retornos:
+            response (str): resposta HTTP do servidor
+    """
     http_version = "HTTP/1.1"
     host = "127.0.0.1:50000"
     user_agent = "server-conces-energia"
